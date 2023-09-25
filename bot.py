10fa2ebe869b9bebecbe7e69c551d9c30fa32f98
bulbaME <65255962+bulbaME@users.noreply.github.com>
2023-09-25 17:04:41 +0300

    update

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import logging
 import telegram
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, JobQueue
+from telegram.ext import ApplicationBuilder, ContextTypes, JobQueue, CommandHandler, MessageHandler, filters
 import yaml
 import updates
 
@@ -13,15 +13,48 @@ logging.basicConfig(
 CRED = yaml.safe_load(open('credentials.yaml'))['TELEGRAM']
 TOKEN = CRED['TOKEN']
 CHAT_ID = CRED['CHAT_ID']
+MONK_ID = CRED['MONK_ID']
 
 async def send_updates(context: ContextTypes.DEFAULT_TYPE):
+    try:
+        if not context.bot_data['logged']:
+            return
+    except:
+        pass
+
     u = updates.get_updates()
+
+    if u == None:
+        driver = updates.get_driver()
+        driver.get(updates.URL)
+        context.bot_data['driver'] = driver
+        context.bot_data['logged'] = False
+        updates.login(driver)
+        await context.bot.send_message(MONK_ID, 'Send authenthication code')
+
+        return
+
+    context.bot_data['logged'] = True
 
     s = ''
     for update in u:
         s += f'{update[0]}\n{update[1]}\nDate: {update[2]}\n\n'
 
-    await context.bot.send_message(CHAT_ID, s)
+    if len(s) > 0:
+        await context.bot.send_message(CHAT_ID, s)
+
+async def auth_code(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if context.bot_data['logged']:
+        return
+    
+    code = update.message.text.strip()
+
+    if not updates.enter_passcode(context.bot_data['driver'], code):
+        await context.bot.send_message(MONK_ID, 'Wrong code, try again')
+        return
+    
+    context.bot_data['logged'] = True
+    context.bot_data['driver'].quit()
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     print(update.effective_chat.id)
@@ -29,6 +62,9 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 def init():
     application = ApplicationBuilder().token(TOKEN).build()
     application.job_queue.run_repeating(send_updates, 300)
+
+    auth_code_hndl = MessageHandler(filters.TEXT, auth_code)
+    application.add_handler(auth_code_hndl)
 
     # start_hndl = CommandHandler('start', start)
     # application.add_handler(start_hndl)
--- updates.py
+++ updates.py
@@ -9,37 +9,102 @@ import yaml
 from datetime import datetime
 
 URL = 'https://smm-heaven.net/admin/updates'
-ADMIN_HASH = yaml.safe_load(open('credentials.yaml'))['SMMHEAVEN']['ADMIN_HASH']
-LAST_UPDATE = datetime.timestamp(datetime.strptime('2023-09-22 07:56:37', '%Y-%m-%d %H:%M:%S'))
+ADMIN_CRED = yaml.safe_load(open('credentials.yaml'))['SMMHEAVEN']['ADMIN']
+LAST_UPDATE = datetime.timestamp(datetime.strptime(open('lastupdate.time').readline(), '%Y-%m-%d %H:%M:%S'))
+MAX_WAIT_TIME = 10
 
-def get_updates() -> list[(str, str, str)]:
+def get_admin_hash() -> str | None:
+    try:
+        fr = open('admin.hash')
+        s = fr.readline()
+        fr.close()
+        
+        return s
+    except BaseException:
+        return None
+
+def set_admin_hash(h: str):
+    fw = open('admin.hash', 'w')
+    fw.write(h)
+    fw.close()
+
+def get_driver() -> webdriver.Firefox:
     driver_opt = Options()
     driver_service = Service(executable_path='./geckodriver.exe')
     # driver_opt.headless = True
     driver_opt.add_argument("--window-size=800,800")
     driver_opt.page_load_strategy = 'eager'
 
-    driver = webdriver.Firefox(options=driver_opt, service=driver_service)
+    return webdriver.Firefox(options=driver_opt, service=driver_service)
 
-    driver.get(URL)
-    driver.add_cookie({'name': 'admin_hash', 'value': ADMIN_HASH})
-    driver.get(URL)
-
-    table: WebElement = WebDriverWait(driver, 15).until(
-        expected_conditions.presence_of_element_located((By.TAG_NAME, 'table'))
+def login(driver: webdriver.Firefox):
+    form: WebElement = WebDriverWait(driver, MAX_WAIT_TIME).until(
+        expected_conditions.presence_of_element_located((By.TAG_NAME, 'form'))
     )
+    
+    inputs = form.find_elements(by=By.TAG_NAME, value='input')
+
+    inputs = list(filter(lambda x: x.is_displayed(), inputs))
+
+    inputs[0].send_keys(ADMIN_CRED['LOGIN'])
+    inputs[1].send_keys(ADMIN_CRED['PSW'])
+
+    form.submit()
+
+def enter_passcode(driver: webdriver.Firefox, passcode) -> bool:
+    form = driver.find_element(by=By.TAG_NAME, value='form')
+    inputs = form.find_elements(by=By.TAG_NAME, value='input')
+
+    inputs = list(filter(lambda x: x.is_displayed(), inputs))
+
+    inputs[0].send_keys(passcode)
+    form.submit()
+
+    try:
+        WebDriverWait(driver, MAX_WAIT_TIME).until(
+            expected_conditions.presence_of_element_located((By.TAG_NAME, 'table'))
+        )
+    except BaseException:
+        return False
+    
+    h = driver.get_cookie('admin_hash')['value']
+
+    set_admin_hash(h)
+
+    return True
+
+def get_updates() -> list[(str, str, str)]:
+    driver = get_driver()
+
+    driver.get(URL)
+    driver.add_cookie({'name': 'admin_hash', 'value': get_admin_hash()})
+    driver.get(URL)
+
+    table = None
+    try:
+        table: WebElement = WebDriverWait(driver, MAX_WAIT_TIME).until(
+            expected_conditions.presence_of_element_located((By.TAG_NAME, 'table'))
+        )
+    except BaseException:
+        pass
+
+    if table == None:
+        driver.quit()
+        return None
 
     table = table.find_element(by=By.TAG_NAME, value='tbody')
     children = table.find_elements(by=By.XPATH, value='*')
     
     updates = []
     new_time = None
+    new_time_s = None
     for c in children:
         elems = c.find_elements(by=By.XPATH, value='*')
         time_s = elems[2].text
         _time = datetime.strptime(time_s, '%Y-%m-%d %H:%M:%S')
         stamp = datetime.timestamp(_time)
         if new_time == None: new_time = stamp
+        if new_time_s == None: new_time_s = stamp
 
         if stamp <= LAST_UPDATE:
             break
@@ -50,6 +115,9 @@ def get_updates() -> list[(str, str, str)]:
         updates.append((service, change, time_s))
 
     LAST_UPDATE = new_time
+    fw = open('lastupdate.time', 'w')
+    fw.write(new_time_s)
+    fw.close()
 
     driver.quit()
 
