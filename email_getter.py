3ef5c88b1d5d8dd5c2c6fd005c62a2dcd1207e01
bulbaME <65255962+bulbaME@users.noreply.github.com>
2023-10-09 13:58:02 +0300

    Update email_getter.py

--- email_getter.py
+++ email_getter.py
@@ -1,21 +1,27 @@
 import email
 import imaplib
 import yaml
+from time import time
 
 CRED = yaml.safe_load(open('credentials.yaml'))['MAIL']
 
 EMAIL = CRED['NAME']
 PASSWORD = CRED['PSW']
 SERVER = 'mail.seodigital360.com'
+MAX_WAIT_TIME = 60
 
 def get_mail_code():
     mail = imaplib.IMAP4_SSL(SERVER)
     mail.login(EMAIL, PASSWORD)
     mail.select('inbox')
+    tf = time()
 
     while True:
         status, data = mail.search(None, '(UNSEEN)')
         mail_ids = []
+
+        tl = time()
+        if (tf + MAX_WAIT_TIME < tl): break
 
         for block in data:
             mail_ids += block.split()
@@ -32,3 +38,5 @@ def get_mail_code():
                         psw = msg.split(':')[1].strip()
                         
                         return int(psw)
+                    
+    return 100000
